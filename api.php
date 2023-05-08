fb9644b9b57cb74d8e488766c4430807c0643814
fabiodelli <ntqdel@hotmail.com>
2023-05-08 16:26:09 +0200

    add app.js

--- api.php
+++ api.php
@@ -1,1 +1,35 @@
 
+<?php
+$filename = 'tasks.json';
+
+function readTasks()
+{
+    global $filename;
+    if (!file_exists($filename)) {
+        return [];
+    }
+    $content = file_get_contents($filename);
+    return json_decode($content, true);
+}
+
+function writeTasks($tasks)
+{
+    global $filename;
+    $content = json_encode($tasks);
+    file_put_contents($filename, $content);
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $tasks = readTasks();
+    header('Content-Type: application/json');
+    echo json_encode($tasks);
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = json_decode(file_get_contents('php://input'), true);
+    $tasks = readTasks();
+    $newTask = [
+        'id' => uniqid(),
+        'title' => $data['title']
+    ];
+    $tasks[] = $newTask;
+    writeTasks($tasks);
+}
---
+++ app.js
@@ -0,0 +1,38 @@
+
+const { createApp } = Vue;
+createApp({
+  data() {
+    return {
+        tasks: [],
+        newTask: "",
+    };
+  },
+  methods: {
+    fetchTasks() {
+        axios
+          .get("api.php")
+          .then((response) => {
+            this.tasks = response.data;
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      },
+      addTask() {
+        if (this.newTask) {
+          axios
+            .post("api.php", { title: this.newTask })
+            .then(() => {
+              this.newTask = "";
+              this.fetchTasks();
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+        }
+      },
+  },
+  mounted() {
+    this.fetchTasks();
+  }
+}).mount("#app");

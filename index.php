299ded0ea5e9f9fcc9221b9609e7dbec430a14c4
fabiodelli <ntqdel@hotmail.com>
2023-05-08 15:18:49 +0200

    add form to index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title></title>
+<link rel="stylesheet" href="./assets/css/style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" 
+integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" 
+crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
+
+<h1>Todo List</h1>
+
+  <form action="">
+    <input type="text" class="form-control w-25" placeholder="add task" aria-label="Recipient's username" aria-describedby="button-addon2">
+    <button class="btn btn-outline-secondary mt-3" type="button" id="button-addon2">Button</button>
+  </form>
+
+<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+<script src="./assets/js/main.js"></script>
+</body>
+</html>
